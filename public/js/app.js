4a7a9e251912123d36d61987792586bc126dafe7
Darren <darren@darrenwhitlen.com>
2014-01-19 23:30:45 +0000

    Namespace browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 $(function() {
+	!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.TinyEmitter=e()}}(function(){var e,n,t;return function r(e,n,t){function i(o,u){if(!n[o]){if(!e[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(f)return f(o,!0);throw new Error("Cannot find module '"+o+"'")}var s=n[o]={exports:{}};e[o][0].call(s.exports,function(n){var t=e[o][1][n];return i(t?t:n)},s,s.exports,r,e,n,t)}return n[o].exports}var f=typeof require=="function"&&require;for(var o=0;o<t.length;o++)i(t[o]);return i}({1:[function(e,n,t){function r(){}r.prototype={on:function(e,n,t){var r=this.e||(this.e={});(r[e]||(r[e]=[])).push({fn:n,ctx:t});return this},once:function(e,n,t){var r=this;var i=function(){r.off(e,i);n.apply(t,arguments)};return this.on(e,i,t)},emit:function(e){var n=[].slice.call(arguments,1);var t=(this.e[e]||[]).slice();var r=0;var i=t.length;for(r;r<i;r++){t[r].fn.apply(t[r].ctx,n)}return this},off:function(e,n){var t=this.e||(this.e={});var r=t[e];var i=[];if(r&&n){for(var f=0,o=r.length;f<o;f++){if(r[f].fn!==n)i.push(r[f])}}i.length?t[e]=i:delete t[e];return this}};n.exports=r},{}]},{},[1])(1)});
+
 	var control = (function() {
 		var self = this;
 
@@ -28,8 +30,8 @@ $(function() {
 		// These options define what this chart shows
 		self.info = info || {
 			title: '',
-			bucket: '',
-			properties: ['count', 'sum', 'mean', 'upper', 'lower'],
+			buckets: [],
+			//buckets: ['website.login count', 'website.logout count'],
 
 			interval: '_global',
 			time_span: ['_global', '_global'], // [time_from, time_to]
@@ -58,11 +60,6 @@ $(function() {
 	Chart.prototype.addUiEvents = function() {
 		var self = this;
 
-		self.$el.on('change', '.bucket-available', function(event) {
-			self.info.bucket = $(this).val();
-			self.refreshChart();
-		});
-
 		self.$el.on('change', '.bucket-interval', function(event) {
 			self.info.interval = $(this).val();
 			self.refreshChart();
@@ -85,44 +82,105 @@ $(function() {
 		self.$el.on('click', '.refresh-chart', function(event) {
 			self.refreshChart();
 		});
+
+		self.$el.on('click', '.add-bucket', function(event) {
+			if (self.ns_browser) {
+				self.ns_browser.dispose();
+				self.ns_browser.off('bucket_change');
+				delete self.ns_browser;
+				return;
+			}
+
+			self.ns_browser = new NamespaceBrowser();
+			self.ns_browser.setSelected(self.info.buckets);
+
+			self.ns_browser.on('bucket_change', function(bucket_name, selected) {
+				var index_of_bucket = self.info.buckets.indexOf(bucket_name);
+
+				if (selected && index_of_bucket === -1) {
+					self.info.buckets.push(bucket_name);
+					console.log('adding ' + bucket_name);
+				} else if (!selected && index_of_bucket > -1) {
+					self.info.buckets.splice(index_of_bucket, 1);
+					console.log('removing ' + bucket_name);
+				}
+			});
+
+			self.ns_browser.$el.appendTo(self.$el.find('.the-chart'));
+		});
 	};
 
 
-	Chart.prototype.refreshChart = function() {
-		var self = this;
-		var properties = self.info.properties;
-		var url_params;
+	Chart.prototype.uniqueBuckets = function() {
+		var buckets = [];
 
-		if (typeof properties === 'string') {
-			properties = [properties];
-		} else if (!properties) {
-			properties = ['SUM'];
+		for (var idx in this.info.buckets) {
+			var parts = this.info.buckets[idx].split(' ');
+			if (!parts[1])
+				parts[1] = 'sum';
+
+			if (buckets.indexOf(parts[0]) === -1)
+				buckets.push(parts[0]);
 		}
 
-		url_params = {
+		return buckets;
+	};
+
+
+	Chart.prototype.getBucketData = function(callback) {
+		var self = this;
+		var buckets = this.uniqueBuckets();
+		var bucket_data = {};
+		var loaded_buckets = 0;
+
+		var url_params = {
 			i:    self.info.interval === '_global' ? control.filter.interval : self.info.interval,
 			from: self.info.time_span[0] === '_global' ? control.filter.time_span[0] : self.info.time_span[0],
 			to:   self.info.time_span[1] === '_global' ? control.filter.time_span[1] : self.info.time_span[1]
 		};
 
+		var loadData = function(bucket_name) {
+			$.getJSON('/buckets/' + bucket_name, url_params, function(json) {
+				bucket_data[bucket_name] = json;
+				loaded_buckets++;
+
+				if (loaded_buckets === buckets.length)
+					callback(bucket_data);
+			});
+		};
+
+		for (var i in buckets)
+			loadData(buckets[i]);
+	};
+
+
+	Chart.prototype.refreshChart = function() {
+		var self = this;
+
 		self.chart.showLoading();
 
-		$.getJSON('/buckets/' + self.info.bucket, url_params, function (json) {
+		this.getBucketData(function(bucket_data) {
 			var series = [];
 			var series_by_name = {};
 			var chart_series_by_name = {};
-			var idx;
 
-			for(var property_idx in properties) {
+			for (var idx in self.info.buckets) {
+				var parts = self.info.buckets[idx].split(' ');
+				if (!parts[1])
+					parts[1] = 'sum';
+
+				var bucket_name = parts[0];
+				var property = parts[1];
+
 				var data = {
-					name: properties[property_idx],
+					name: bucket_name + ' - ' + property,
 					data: []
 				};
 
-				for (idx in json) {
+				for (var json_idx in bucket_data[bucket_name]) {
 					data.data.push([
-						json[idx].time * 1000,
-						json[idx][properties[property_idx]]
+						bucket_data[bucket_name][json_idx].time * 1000,
+						bucket_data[bucket_name][json_idx][property]
 					]);
 				}
 
@@ -145,11 +203,12 @@ $(function() {
 				}
 			}
 
-			self.chart.setTitle({text: self.info.title || self.info.bucket});
+			self.chart.setTitle({text: self.info.title || ''});
 
 			self.chart.hideLoading();
 		});
 	};
+
 
 
 	Chart.prototype.initChart = function($el) {
@@ -179,7 +238,7 @@ $(function() {
 			},
 			tooltip: {
 				formatter: function() {
-						return '<b>'+ (self.info.title || self.info.bucket) + ' - ' + this.series.name +'</b><br/>'+
+						return '<b>' + this.series.name + '</b><br/>'+
 						'<i>'+Highcharts.dateFormat('%b %e, %Y %H:%M:%S', this.x) + '</i> = ' + Highcharts.numberFormat(this.y);
 				}
 			},
@@ -191,6 +250,106 @@ $(function() {
 
 		return $el.highcharts();
    };
+
+
+
+
+	function NamespaceBrowser() {
+		var self = this;
+
+		self.$el = $($('#tmpl_namespace_browser').html());
+		self.addUiEvents();
+
+		this.selected = {};
+		self.loadNamespace(self.$el);
+	}
+	NamespaceBrowser.prototype = new TinyEmitter();
+
+
+	NamespaceBrowser.prototype.dispose = function() {
+		this.$el.remove();
+	};
+
+
+	NamespaceBrowser.prototype.addUiEvents = function() {
+		var self = this;
+
+		this.$el.on('click', '.namespace label', function(event) {
+			event.stopPropagation();
+
+			var $target_namespace = $(this).parent();
+			var $child_namespaces = $target_namespace.find('ul');
+
+			if ($child_namespaces.length) {
+				$child_namespaces.remove();
+			} else {
+				self.loadNamespace($target_namespace, $target_namespace.data('bucket'));
+			}
+		});
+
+		this.$el.on('click', '.bucket label', function(event) {
+			event.stopPropagation();
+
+			var $item = $(this).parent();
+			var bucket_name = $item.data('bucket');
+			var is_checked = !!($item.find('input').is(':checked'));
+
+			self.emit('bucket_change', bucket_name, is_checked);
+
+			if (!is_checked) {
+				delete self.selected[bucket_name];
+			} else {
+				self.selected[bucket_name] = true;
+			}
+		});
+	};
+
+
+	NamespaceBrowser.prototype.setSelected = function(selected) {
+		selected = selected || [];
+
+		this.selected = {};
+		for (var idx in selected)
+			this.selected[selected[idx]] = true;
+	};
+
+
+	NamespaceBrowser.prototype.loadNamespace = function($target_namespace, namespace_name) {
+		var self = this;
+
+		$.getJSON('/buckets/', {namespace: namespace_name}, function(json) {
+			if (!json || !json.length)
+				return;
+
+			var parent_namespace = namespace_name ? namespace_name + '.' : '';
+			var $namespace = $('<ul></ul>');
+
+			var item, $item, $checkbox;
+			for (var idx in json) {
+				item = json[idx];
+				$item = $('<li class="item"></li>')
+					.data('bucket', parent_namespace + item.namespace);
+
+				$item.append($('<label></label>').text(item.namespace));
+				$item.addClass(item.is_bucket ? 'bucket' : 'namespace');
+
+				if (item.is_bucket) {
+					$checkbox = $('<label><input type="checkbox" /></label>').append(item.namespace);
+					$checkbox.find('input').attr('name', parent_namespace + item.namespace);
+					$item.empty().prepend($checkbox);
+
+					if (self.selected[parent_namespace + item.namespace])
+						$item.find('input').attr('checked', 'checked');
+				}
+
+				$namespace.append($item);
+			}
+
+			$target_namespace.append($namespace);
+		});
+	};
+
+	window.NamespaceBrowser = NamespaceBrowser;
 
 
 
